57d1dfc7cc52f8b2edfe12e7315a97b7cb76d55e
zengodgr <zengodgr@gmail.com>
2025-10-06 14:11:39 +0300

    yey

--- lola/src/main.js
+++ lola/src/main.js
@@ -2,6 +2,9 @@ import { createApp } from "vue";
 import Overlay from "./Overlay.vue";
 import { check } from '@tauri-apps/plugin-updater';
 import { relaunch } from '@tauri-apps/plugin-process';
+
+createApp(Overlay).mount("#app");
+
 try {
     const update = await check();
     if (update) {
@@ -31,4 +34,3 @@ try {
         await relaunch();
     }
 } catch (error) { console.log(error) }
-createApp(Overlay).mount("#app");
